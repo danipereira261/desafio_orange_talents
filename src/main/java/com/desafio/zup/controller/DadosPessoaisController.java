80d21608e381d3ed8a2e61ad68717fdb911e7e9c
danipereira261 <dpereira261@gmail.com>
2021-01-07 15:22:57 -0300

    tratamento de mensagens

--- src/main/java/com/desafio/zup/controller/DadosPessoaisController.java
+++ src/main/java/com/desafio/zup/controller/DadosPessoaisController.java
@@ -4,7 +4,11 @@ package com.desafio.zup.controller;
 import com.desafio.zup.dto.DadosPessoaisDto;
 import com.desafio.zup.model.DadosPessoaisModel;
 import com.desafio.zup.repository.DadosPessoaisRepository;
+import jakarta.validation.Valid;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +21,8 @@ public class DadosPessoaisController {
     @Autowired
     private DadosPessoaisRepository repository;
 
-    @PostMapping
-    public String cadastrar(@RequestBody DadosPessoaisDto dadosPessoaisDto) {
+    @PostMapping(value = "/cad")
+    public ResponseEntity<DadosPessoaisDto> cadastrar(@RequestBody @Valid DadosPessoaisDto dadosPessoaisDto) {
 
         DadosPessoaisModel data = new DadosPessoaisModel();
         data.setNome(dadosPessoaisDto.getNome());
@@ -26,12 +30,13 @@ public class DadosPessoaisController {
         data.setEmail(dadosPessoaisDto.getEmail());
         data.setDataNascimento(dadosPessoaisDto.getDataNascimento());
 
-
-
-
-        repository.save(data);
+        try {
+            repository.save(data);
+        }catch (Exception e){
+            throw new PSQLException("Dados duplicados");
+        }
         System.out.println(dadosPessoaisDto);
-        return "Sucesso";
+        return new ResponseEntity<DadosPessoaisDto>(HttpStatus.OK);
 
     }
 }
--- src/main/java/com/desafio/zup/dto/DadosPessoaisDto.java
+++ src/main/java/com/desafio/zup/dto/DadosPessoaisDto.java
@@ -1,5 +1,7 @@
 package com.desafio.zup.dto;
 
+import com.sun.istack.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DadosPessoaisDto {
 
+    @NotNull
+    @NotEmpty
     private String nome;
+
+    @NotNull
+    @NotEmpty
     private String email;
+
+    @NotNull
+    @NotEmpty
     private String cpf;
+
+    @NotNull
+    @NotEmpty
     private String dataNascimento;
 }
